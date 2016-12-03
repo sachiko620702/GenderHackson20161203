84b877497ec29e1e424eaf793d21e63661341adc
sachiko <sachiko@gmail.com>
2016-12-03 16:25:02 +0800

    no message

--- GenderHackson20161203/GenderHackson20161203/ViewController.m
+++ GenderHackson20161203/GenderHackson20161203/ViewController.m
@@ -20,7 +20,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
-    NSURL *url = [NSURL URLWithString:@"http://sachiko620702.github.io/GenderHackson20161203/?jrie3"];
+    NSURL *url = [NSURL URLWithString:@"http://sachiko620702.github.io/GenderHackson20161203/?jri3"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [self.MyWeb loadRequest:request];
 }
