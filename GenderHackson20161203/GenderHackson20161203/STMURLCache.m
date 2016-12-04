778d19d5f83c42194f683b3287a55d4fb38c6913
sachiko <sachiko@gmail.com>
2016-12-04 09:50:32 +0800

    no message

---
+++ GenderHackson20161203/GenderHackson20161203/STMURLCache.m
@@ -0,0 +1,9 @@
+//
+//  STMURLCache.m
+//  GenderHackson20161203
+//
+//  Created by jiayu on 2016/12/4.
+//  Copyright © 2016年 GenderHackson20161203. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
